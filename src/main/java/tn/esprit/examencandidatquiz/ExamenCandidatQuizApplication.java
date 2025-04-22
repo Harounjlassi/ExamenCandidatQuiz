307c50f0afdce7bb5452ddbfde90fe0f22b4adf8
Harounjlassi <jlassi.haroun@esprit.tn>
2025-04-22 12:01:47 +0100

    add: emtyProject

---
+++ src/main/java/tn/esprit/examencandidatquiz/ExamenCandidatQuizApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.examencandidatquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenCandidatQuizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenCandidatQuizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/examencandidatquiz/ExamenCandidatQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.examencandidatquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenCandidatQuizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
